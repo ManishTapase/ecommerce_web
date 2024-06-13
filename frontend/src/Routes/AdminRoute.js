b82c25afa4e8777f2ac94b53f885a41bd2b2932e
ManishTapase <144148125+ManishTapase@users.noreply.github.com>
2024-06-13 22:12:54 +0530

    Update AdminRoute.js

--- frontend/src/Routes/AdminRoute.js
+++ frontend/src/Routes/AdminRoute.js
@@ -3,12 +3,13 @@ import { useAuth } from '../Contexts/AuthContext';
 import axios from 'axios';
 import { Outlet } from 'react-router-dom';
 import Spinner from '../spinner/Spinner';
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 const AdminRoute = () => {
     const [auth,setAuth] = useAuth("");
     const [ok,setOk] = useState(false);
     useEffect(()=>{
         const checkAuth = async()=>{
-            const result = await axios.get("/api/v1/auth/admin-auth");
+            const result = await axios.get(`${backendUrl}/api/v1/auth/admin-auth`);
             if(result.data.ok){
                 setOk(true);
             }else{
