b3a9e33902ad4f887c6d64ccdc967b71a9f68389
ManishTapase <144148125+ManishTapase@users.noreply.github.com>
2024-06-13 22:13:39 +0530

    Update PrivateRoute.js

--- frontend/src/Routes/PrivateRoute.js
+++ frontend/src/Routes/PrivateRoute.js
@@ -3,12 +3,13 @@ import { useAuth } from '../Contexts/AuthContext';
 import axios from 'axios';
 import { Outlet } from 'react-router-dom';
 import Spinner from '../spinner/Spinner';
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 const PrivateRoute = () => {
     const [auth,setAuth] = useAuth("");
     const [ok,setOk] = useState(false);
     useEffect(()=>{
         const checkAuth = async()=>{
-            const result = await axios.get("/api/v1/auth/user-auth");
+            const result = await axios.get(`${backendUrl}/api/v1/auth/user-auth`);
             if(result.data.ok){
                 setOk(true);
             }else{
